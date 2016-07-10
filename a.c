1ed58f20d874ad6727313a31468a98d5fcfc933b
vaishnave <vaishnavekpr13@gmail.com>
2016-07-10 11:45:39 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,26 +1,31 @@
-# include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int a[]={5,7,9,4,1};
-int temp,i,j;
-for(i=0;i<5;i++)
+int arr[100];
+int size, i, j, temp;
+printf("Enter size of array: ");
+scanf("%d", &size);
+printf("Enter elements in array: ");
+for(i=0; i<size; i++)
 {
-temp=a[i];
-for(j=1;j<5;j++)
-{
-j=i-1;
-while(temp>a[j] && j>=0)
-{
-a[j+1]=a[j];
---j;
+scanf("%d", &arr[i]);
 }
-a[j+1]=temp;
-}
-printf("in descending order:");
-}
-for(i=0;i<5;i++);
+for(i=0; i<size; i++)
 {
-printf("%d",&a[i]);
+for(j=i+1; j<size; j++)
+{
+if(arr[j] < arr[i])
+{
+temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
+}
+}
+}
+printf("\nElements of array in sorted ascending order: ");
+for(i=0; i<size; i++)
+{
+printf("%d\t", arr[i]);
 }
 return 0;
-}
+} 
