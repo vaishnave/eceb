ac3eeaee631888ddb07dec371b4e482bb9d37ba8
vaishnave <vaishnavekpr13@gmail.com>
2016-07-10 12:39:03 +0530

    Update fg.c

--- fg.c
+++ fg.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-int n, i,a;
+int n, i,a,b;
 printf("Enter an integer: ");
 scanf("%d",&n);
 for(i=1; i<=10; ++i)
 {
 a=n*i;  
 printf(" %d \n",a);
-
+if(a%2==0&&2n-1==3)
+printf("pair up the childrens");
 }
 return 0;
 }
